f93dc7b1bd8bdb46b80d3ecaa1bdaf63656e3d63
Sajid Ullah <sajidullah124@gmail.com>
2018-11-07 18:08:11 +0500

    Add files via upload

---
+++ check_internetconnection.py
@@ -0,0 +1,14 @@
+"""
+Created on Wed Nov  7 18:02:35 2018
+
+@author: sajid Ullah
+"""
+
+import urllib.request
+
+try:
+    urllib.request.urlopen("http://google.com", timeout=2)
+    print ("working connection")
+
+except urllib2.URLError:
+    print ("No internet connection")
